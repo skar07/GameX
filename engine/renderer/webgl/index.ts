f3f8c69082f3955a92b597e3477d2d1e93112cf5
skar07 <sanskarpandey0@gmail.com>
2025-09-09 19:17:10 +0530

    fix: correct typos for gl_Position

--- engine/renderer/webgl/index.ts
+++ engine/renderer/webgl/index.ts
@@ -25,7 +25,7 @@ export class WebGLRenderer {
         uniform mat4 uModelViewMatrix;
         uniform mat4 uProjectionMatrix;
         void main() {
-          gl_position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+          gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
         }
       `;
       const fsSource = `
